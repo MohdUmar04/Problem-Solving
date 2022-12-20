cabf58e5c88e4fc297831fbdb7d6d9cb9487ecef
Mohd Umar <112498005+MohdUmar04@users.noreply.github.com>
2022-12-20 11:27:03 +0530

    Added Merge K sorted Lists

---
+++ PriorityQueue/MergeKsortedLists.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.*;
+class Solution {
+    public ListNode inatbeg(ListNode l,Integer a)
+    {
+        if(l!=null){
+        ListNode n=new ListNode(a);
+        ListNode st=l;
+        ListNode t=n;
+        while(st.next!=null)
+        st=st.next;
+        st.next=t;}
+        else
+        {
+            ListNode n=new ListNode(a,null);
+            l=n;
+        }
+        return l;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode t=null;
+    PriorityQueue<Map.Entry<Integer,ListNode>> minheap = new PriorityQueue<>((a,b)->a.getValue().val-b.getValue().val);
+    HashMap<Integer,ListNode> map=new HashMap<>();
+    int i;
+        for(i=0;i<lists.length;i++)
+        {
+            if(lists[i]!=null)
+            map.put(i,lists[i]);
+        }
+        for(Map.Entry<Integer,ListNode> entry : map.entrySet()) {
+			minheap.offer(entry);
+		}
+        Map.Entry<Integer,ListNode> e;
+        while(!minheap.isEmpty())
+        {
+            
+            e=minheap.poll();
+            Integer j=e.getKey();
+            Integer z=e.getValue().val;
+            System.out.println(z);
+            t=inatbeg(t,e.getValue().val);
+             if(lists[j].next!=null){
+                 lists[j]=lists[j].next;
+               e.setValue(lists[j]);
+               minheap.offer(e);
+            }
+
+        }
+        return t;
+    }
+}

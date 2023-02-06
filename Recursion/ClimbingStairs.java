676a5e8334eb6e957b3ab68433103b5a14c86bd7
Mohd Umar <112498005+MohdUmar04@users.noreply.github.com>
2023-02-06 22:16:38 +0530

    Recursion

---
+++ Recursion/ClimbingStairs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ClimbingStairs {
+    public static int solve(int n,int current) {
+        if(current==n) return 1;
+        if(current>n) return 0;
+        int left=solve(n, current+1);
+        int right=solve(n, current+2);
+        return left+right;
+    }
+
+    public static int find(int n)
+    {
+        return solve(n,0);
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(find(n));
+    }
+}

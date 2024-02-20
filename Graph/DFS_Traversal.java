30a7100ead29856de07d81d316203796f6038776
Mohd Umar <112498005+MohdUmar04@users.noreply.github.com>
2024-02-20 18:03:45 +0530

    Graph update

--- Graph/DFS_Traversal.java
+++ Graph/DFS_Traversal.java
@@ -19,56 +19,16 @@ public class DFS_Traversal
                 dfs(vertex,visited,d);
         }
     }
-    private static void bfs(int v,boolean[] visited,List<Integer> b){
-        visited[v]=true;
-        Queue<Integer> que=new LinkedList<>();
-        que.add(v);
-        while(!que.isEmpty()){
-
-            int s=que.size();
-            while(s-->0){
-                int t=que.poll();
-                b.add(t);
-                for(int vertex: graph.get(t)){
-                    if(!visited[vertex]){
-                        visited[vertex]=true;
-                        que.add(vertex);
-                    }
-                }
-            }
-
-        }
-    }
-    private static void count(int v,List<List<Integer>> components) {
-        boolean[] visited= new boolean[v];
-        Arrays.fill(visited,false);
-        for(int i=0;i<v;i++){
-            
-            List<Integer> res= new ArrayList<>();
-            if(!visited[i])
-            bfs(i,visited,res);
-            if(res.size()>0)
-            components.add(res);
-        }
-        
-    }
     public static void main(String[] args) {
         int v=9;
         int[][] edges={{1,7},{1,3},{1,2},{2,3},{5,6},{6,8}};
         graph=new ArrayList<>();
         createGraph(edges,v);
-        System.out.println(graph);
+        System.out.println("Graph=" +graph);
         boolean[] visited=new boolean[v];
         Arrays.fill(visited,false);
         List<Integer> d=new ArrayList<>();
-        dfs(0,visited,d);
+        dfs(1,visited,d);
         System.out.println("DFS: "+(d));
-        Arrays.fill(visited,false);
-        List<Integer> b=new ArrayList<>();
-        bfs(0,visited,b);
-        System.out.println("BFS: "+(b));
-        List<List<Integer>> components=new ArrayList<>();
-        count(v,components);
-        System.out.println(components);
     }
 }
--- Graph/FindAllComponents.java
+++ Graph/FindAllComponents.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class BFS_Traversal
+public class FindAllComponents
 {
     static List<List<Integer>> graph;
     private static void createGraph(int[][] edges,int v){
@@ -11,7 +11,14 @@ public class BFS_Traversal
             graph.get(edge[1]).add(edge[0]);
         }
     }
-
+    private static void dfs(int v,boolean[] visited,List<Integer> d){
+        visited[v]=true;
+        d.add(v);
+        for(int vertex : graph.get(v)){
+            if(!visited[vertex])
+                dfs(vertex,visited,d);
+        }
+    }
     private static void bfs(int v,boolean[] visited,List<Integer> b){
         visited[v]=true;
         Queue<Integer> que=new LinkedList<>();
@@ -32,7 +39,19 @@ public class BFS_Traversal
 
         }
     }
-   
+    private static void count(int v,List<List<Integer>> components) {
+        boolean[] visited= new boolean[v];
+        Arrays.fill(visited,false);
+        for(int i=0;i<v;i++){
+            
+            List<Integer> res= new ArrayList<>();
+            if(!visited[i])
+            bfs(i,visited,res);
+            if(res.size()>0)
+            components.add(res);
+        }
+        
+    }
     public static void main(String[] args) {
         int v=9;
         int[][] edges={{1,7},{1,3},{1,2},{2,3},{5,6},{6,8}};
@@ -41,8 +60,15 @@ public class BFS_Traversal
         System.out.println(graph);
         boolean[] visited=new boolean[v];
         Arrays.fill(visited,false);
+        List<Integer> d=new ArrayList<>();
+        dfs(1,visited,d);
+        System.out.println("DFS: "+(d));
+        Arrays.fill(visited,false);
         List<Integer> b=new ArrayList<>();
         bfs(1,visited,b);
         System.out.println("BFS: "+(b));
+        List<List<Integer>> components=new ArrayList<>();
+        count(v,components);
+        System.out.println(components);
     }
 }

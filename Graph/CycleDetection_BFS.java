7605f11633a896b0d1a3d95b994d734d3d7de285
Mohd Umar <112498005+MohdUmar04@users.noreply.github.com>
2024-02-24 01:19:24 +0530

    update

---
+++ Graph/CycleDetection_BFS.java
@@ -0,0 +1,33 @@
+public class CycleDetection_BFS {
+       
+    
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        HashSet<Integer> vis=new HashSet<>();
+        for(int i=0;i<V;i++){
+            if(vis.contains(i)==false){
+                // System.out.println(i +" "+ vis);
+        Queue<Integer> q=new LinkedList<>();
+        q.add(i);
+        vis.add(i);
+        Map<Integer,Integer> parent=new HashMap<>();
+        parent.put(i,-1);
+       
+        while(!q.isEmpty()){
+            int x=q.poll();
+            for(Integer v:adj.get(x)){
+                if(v==parent.get(x)) continue;
+                if(vis.contains(v)) {
+                    // System.out.println(vis +" "+ v + parent.get(115));
+                    return true;
+                }
+                    q.add(v);
+                    vis.add(v);
+                    parent.put(v,x);
+            
+            }
+        }
+    }
+    }
+     return false;
+    }
+}

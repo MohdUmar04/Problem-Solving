4d68e882404f0b9db1866961d3a99281a2b82b93
Mohd. Umar <mohdumarsiddiqui04@gmail.com>
2024-08-01 10:21:45 +0530

    Time: 5 ms (10.24%), Space: 43.2 MB (10.46%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 class Solution {
     public int countSeniors(String[] details) {          
         return (int) Arrays.stream(details)
-            .filter(a -> Integer.parseInt(a.substring(11, 13)) > 60).count();
+            .filter(a -> Integer.parseInt((new StringBuilder(a)).substring(11, 13)) > 60).count();
             
     }
 }

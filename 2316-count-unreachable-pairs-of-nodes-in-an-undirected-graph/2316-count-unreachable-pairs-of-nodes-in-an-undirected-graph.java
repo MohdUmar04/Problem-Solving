06f6361abeb59033468abd13a219891a45149fb1
Mohd. Umar <mohdumarsiddiqui04@gmail.com>
2024-08-07 14:13:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,51 @@
+class DSU {
+    int[] parent;
+    DSU(int n){
+        parent = new int[n];
+        for(int i=0;i<n;i++)
+            parent[i] = i;
+    }
+
+    int find(int a) {
+        if(a == parent[a]) return a;
+        else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    void union(int a, int b) {
+        int aParent = find(a);
+        int bParent = find(b);
+
+        if(aParent != bParent) {
+            parent[aParent] = bParent;
+        }
+    }
+
+}
+
+
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        DSU dsu = new DSU(n);
+        for(int[] e : edges) {
+            dsu.union(e[0],e[1]);
+        }
+        long res = 0L;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < n ; i++) 
+        {
+            int f = dsu.find(i);
+            map.put(f,map.getOrDefault(f,0)+1);
+        }
+            
+
+        
+
+        for(var v : map.entrySet()) 
+            res += v.getValue() * (n-v.getValue());
+        return res/2;
+    }
+}
